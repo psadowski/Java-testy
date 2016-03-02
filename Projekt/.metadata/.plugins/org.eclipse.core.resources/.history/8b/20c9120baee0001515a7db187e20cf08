858b463ca7eaf69352611b2fa28d7724ea679c7c
Patryk Sadowski <patryk.sadowski113@gmail.com>
2016-03-02 20:38:39 +0100

    Added interface

---
+++ Projekt/.metadata/.plugins/org.eclipse.core.resources/.history/8b/20c9120baee0001515a7db187e20cf08
@@ -0,0 +1,5 @@
+package MavenLab03.Zad01;
+
+public interface MyStack {
+
+}
--- Projekt/Zad01/src/main/java/MavenLab03/Zad01/MyStack.java
+++ Projekt/Zad01/src/main/java/MavenLab03/Zad01/MyStack.java
@@ -1,5 +1,10 @@
 package MavenLab03.Zad01;
 
 public interface MyStack {
-
+	public void MyPush(int val);
+	public void MyPop();
+	public boolean IsNull();
+	public int MyTop();
+	
+	
 }
