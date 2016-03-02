94a7192969ad8cf509ec15fe1f9b3534caae8ae6
Patryk Sadowski <patryk.sadowski113@gmail.com>
2016-03-02 22:23:58 +0100

    Push Exception Test

---
+++ Projekt/Zad01/src/test/java/MavenLab03/Zad01/StackPushExceptionTest.java
@@ -0,0 +1,36 @@
+package MavenLab03.Zad01;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StackPushExceptionTest {
+	private MyStackImpl stack;
+
+	
+	@Before
+	public void setUp() throws Exception {
+		stack = new MyStackImpl();
+	}
+	
+	@Test
+	public void NullStackTest() {
+		stack.setStack(null);
+		try {
+				stack.MyPush(13);
+			} catch (NullPointerException e) {
+				assertTrue(true);
+		}
+	}
+	
+	
+	@After
+	public void tearDown() throws Exception {
+		stack = null;
+	}
+
+	
+}
