1816b63780f53a7c241a8c6c6da673687cfe155c
Patryk Sadowski <patryk.sadowski113@gmail.com>
2016-03-02 20:54:11 +0100

    Implemented setters and getters for stack

--- Projekt/Zad01/src/main/java/MavenLab03/Zad01/MyStackImpl.java
+++ Projekt/Zad01/src/main/java/MavenLab03/Zad01/MyStackImpl.java
@@ -5,6 +5,14 @@ import java.util.List;
 public class MyStackImpl implements MyStack {
 	private List<Integer> stack;
 	
+	public List<Integer> getStack() {
+		return stack;
+	}
+
+	public void setStack(List<Integer> stack) {
+		this.stack = stack;
+	}
+
 	public void MyPush(int val) {
 			stack.add(val);
 	}
@@ -23,8 +31,7 @@ public class MyStackImpl implements MyStack {
 	}
 
 	public int MyTop() {
-		// TODO Auto-generated method stub
-		return 0;
+
 	}
 
 }
