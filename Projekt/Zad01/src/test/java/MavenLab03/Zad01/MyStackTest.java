a471d684c28d80fff787907730a39fad68c18b81
Patryk Sadowski <patryk.sadowski113@gmail.com>
2016-03-02 16:05:57 +0100

    initialize project

---
+++ Projekt/Zad01/src/test/java/MavenLab03/Zad01/MyStackTest.java
@@ -0,0 +1,14 @@
+package MavenLab03.Zad01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyStackTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
