7f06be57501d42c23e959c65cdeca25159f6b1df
Patryk Sadowski <patryk.sadowski113@gmail.com>
2016-03-06 13:39:30 +0100

    method CyfroKrad added

--- Projekt/Zad02/src/main/java/MavenLab03/Zad02/Kontrakt.java
+++ Projekt/Zad02/src/main/java/MavenLab03/Zad02/Kontrakt.java
@@ -1,12 +1,17 @@
 package MavenLab03.Zad02;
-
+import java.util.Random;
 public class Kontrakt implements Psikus {
 /*Metoda CyfroKrad działa w ten sposób, że w liczbie przekazanej jako argument, usuwa
 losowo jedną cyfrę. Gdy jednocyfrowa zwraca null.*/
-	
+	private Random random = new Random();
 	public int CyfroKrad(int liczba) {
-		
-		return 0;
+		if(liczba <= 9 && liczba >= -9){
+			return (Integer) null;
+		} else {
+			String liczbaToString = Integer.toString(liczba);
+			int position = random.nextInt(liczbaToString.length());
+	        return (Integer.parseInt(liczbaToString.substring(0,position)+liczbaToString.substring(position+1)));
+		}  
 	}
 
 	public int HultajChochla(int liczba) throws NieudanyPsikusException {
