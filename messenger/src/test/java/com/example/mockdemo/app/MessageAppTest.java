c91785df1244f6774da0d6b0ce5809f95b8ce411
Patryk Sadowski <patryk.sadowski113@gmail.com>
2016-03-21 13:09:40 +0100

    Added few tests to messenger

--- messenger/src/test/java/com/example/mockdemo/app/MessageAppTest.java
+++ messenger/src/test/java/com/example/mockdemo/app/MessageAppTest.java
@@ -4,24 +4,84 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 
 public class MessageAppTest {
-
-	Messenger messenger = new Messenger();
+	
+	private MessageServiceMock msm;
+	private Messenger msg;
 
 	private final String VALID_SERVER = "inf.ug.edu.pl";
 	private final String INVALID_SERVER = "inf.ug.edu.eu";
-
+	private final String INVALID_SERVER2 ="aa"; 
+	
+	
 	private final String VALID_MESSAGE = "some message";
 	private final String INVALID_MESSAGE = "ab";
 
+	@Before
+	public void setUp() throws Exception {
+		msm = new MessageServiceMock();
+		msg = new Messenger(msm);
+	}
+	
+	@Test 
+	public void CheckShortServerConnection() {
+		assertEquals(1, msg.testConnection(INVALID_SERVER2));
+	}
+	
+	@Test
+	public void CheckNotPlServerConnection() {
+		assertEquals(1, msg.testConnection(INVALID_SERVER));
+	}
+	
+	@Test
+	public void CheckCorrectServerConnection() {
+		assertEquals(0, msg.testConnection(VALID_SERVER));
+	}
+	
+	@Test
+	public void CheckNullServerSendingStatus() {
+		assertEquals(2, msg.sendMessage(null, VALID_MESSAGE));
+	}
+	
+	@Test
+	public void CheckNullMessageSendingStatus() {
+		assertEquals(2, msg.sendMessage(VALID_SERVER, null));
+	}
+	
+	@Test
+	public void CheckBothNullSendingStatus() {
+		assertEquals(2, msg.sendMessage(null, null));
+	}
+	
+	@Test
+	public void CheckNullServerInvalidMsgSendingStatus() {
+		assertEquals(2, msg.sendMessage(null, INVALID_MESSAGE));
+	}
+	
+	@Test
+	public void CheckNullMsgInvalidServerSendingStatus() {
+		assertEquals(2, msg.sendMessage(INVALID_SERVER, null));
+	}
+	
+	
+	
 	@Test
 	public void checkSendingMessage() {
 
-		assertEquals(1, messenger.sendMessage(INVALID_SERVER, VALID_MESSAGE));
-		assertEquals(2, messenger.sendMessage(VALID_SERVER, INVALID_MESSAGE));
+		assertEquals(1, msg.sendMessage(INVALID_SERVER, VALID_MESSAGE));
+		assertEquals(2, msg.sendMessage(VALID_SERVER, INVALID_MESSAGE));
 
-		assertThat(messenger.sendMessage(VALID_SERVER, VALID_MESSAGE),
+		assertThat(msg.sendMessage(VALID_SERVER, VALID_MESSAGE),
 				either(equalTo(0)).or(equalTo(1)));
+	}
+	
+	
+	@After
+	public void tearDown() throws Exception {
+		msg = null;
+		msm = null;
 	}
 }
---
+++ messenger/src/test/java/com/example/mockdemo/app/MessageServiceMock.java
@@ -0,0 +1,42 @@
+package com.example.mockdemo.app;
+
+import com.example.mockdemo.messenger.ConnectionStatus;
+import com.example.mockdemo.messenger.MalformedRecipientException;
+import com.example.mockdemo.messenger.MessageService;
+import com.example.mockdemo.messenger.SendingStatus;
+
+public class MessageServiceMock implements MessageService {
+
+	private boolean messageStatus;
+	
+	@Override
+	public ConnectionStatus checkConnection(String server) {
+		if (!server.endsWith(".pl")) {
+			return ConnectionStatus.FAILURE;
+		}
+		return ConnectionStatus.SUCCESS;
+	}
+	
+	@Override
+	public SendingStatus send(String server, String message)
+			throws MalformedRecipientException {
+
+		if (message == null || message.length() < 3)
+			throw new MalformedRecipientException();
+		
+		if (server == null || server.length() < 4)
+			throw new MalformedRecipientException();
+		
+		if (checkConnection(server) == ConnectionStatus.FAILURE) {
+			return SendingStatus.SENDING_ERROR;
+		}
+
+		if (messageStatus) {
+			return SendingStatus.SENT;
+		}
+		return SendingStatus.SENDING_ERROR;
+	}
+
+
+
+}
