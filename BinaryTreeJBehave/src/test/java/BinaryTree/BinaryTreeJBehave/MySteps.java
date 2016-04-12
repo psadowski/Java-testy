a3853deefd73f6c2610854348d56b2558e89e8e2
Patryk Sadowski <patryk.sadowski113@gmail.com>
2016-04-12 20:57:12 +0200

    ostatni

--- BinaryTreeJBehave/src/test/java/BinaryTree/BinaryTreeJBehave/MySteps.java
+++ BinaryTreeJBehave/src/test/java/BinaryTree/BinaryTreeJBehave/MySteps.java
@@ -22,6 +22,11 @@ public class MySteps extends Steps {
 		impl = new BinaryTreeImpl();
 
 	}
+	
+	@AfterScenario
+	public void end() {
+		impl = null;
+	}
 
 	@Given("a first node to tree,")
 	public void givenAFirstNodeToTree() {
@@ -107,5 +112,127 @@ public class MySteps extends Steps {
 	@When("fining it, it should start finding in leftChild.")
 	public void test() {
 		assertThat(impl.findNode(0), is(not(equalTo(null))));
+	}
+	
+	@Given("a node to delete from tree,")
+	public void usuwanie() {
+		impl.addNode(1, "first");
+		impl.addNode(0, "second");
+		impl.addNode(6, "third");
+		impl.addNode(7, "third");
+		impl.addNode(5, "third");
+		impl.addNode(8, "third");
+		impl.addNode(9, "third");
+	}
+	@Given("a node to delete from tree, which doesnt exsist in,")
+	public void usuwanie2(){
+		impl.addNode(50, "first");
+		impl.addNode(25, "second");
+		impl.addNode(15, "third");
+		impl.addNode(30, "third");
+		impl.addNode(75, "third");
+		impl.addNode(85, "third");
+	}
+	@When("deleting it, remove method should return true.")
+	public void verify(){
+		assertThat(impl.remove(9), is(equalTo(true)));
+		
+	}
+	@When("deleting it, remove method should return false.")
+	public void verify2(){
+		assertThat(impl.remove(11), is(equalTo(false)));
+	}
+	@When("deleting it, we shouldnt find it in tree with findNode method.")
+	public void verify3(){
+		impl.remove(25);
+		assertThat(impl.findNode(25),is(equalTo(null)));
+	}
+	@Given("a root to delete from tree,")
+	public void rootDelete(){
+		impl.addNode(0, "root");
+	}
+	@When("deleting it, we should find null tree.")
+	public void verify4(){
+		impl.remove(0);
+		assertThat(impl.root, is(equalTo(null)));
+	}
+	@Given("a left node with no child to delete,")
+	public void delete(){
+		impl.addNode(2, "root");
+		impl.addNode(1, "left");
+		impl.addNode(3, "right");
+	}
+	@When("deleting it, we should get true from method.")
+	public void verify5(){
+		impl.remove(1);
+		assertThat(impl.root.leftChild, is(equalTo(null)));
+	}
+	@Given("a left node with no right child,")
+	public void delete1(){
+		impl.addNode(2, "root");
+		impl.addNode(1, "left");
+		impl.addNode(3, "right");
+		impl.addNode(4, "child");
+	}
+	@When("deleting it should be deleted.")
+	public void verify6(){
+		assertThat(impl.remove(3), is(equalTo(true)));
+	}
+	@Given("a node with children to delete,")
+	public void delete3(){
+		impl.addNode(2, "root");
+		impl.addNode(1, "left");
+		impl.addNode(3, "right");
+		impl.addNode(4, "child");
+	}
+	@When("deleting it should make replecament.")
+	public void verify7(){
+		assertThat(impl.remove(2), is(equalTo(true)));
+	}
+	@Given("a right node with no child to delete,")
+	public void delete1231(){
+		impl.addNode(2, "root");
+		impl.addNode(1, "left");
+		impl.addNode(10, "right");
+		impl.addNode(0, "root");
+		impl.addNode(6, "root");
+	}
+	@When("deleting it, we should get replacement.")
+	public void verify11(){
+		assertThat(impl.remove(10), is(equalTo(true)));
+	}
+	@When("deleting it, we should get replacement2.")
+	public void verify12(){
+		assertThat(impl.remove(10), is(equalTo(true)));
+		assertThat(impl.remove(6), is(equalTo(true)));
+		assertThat(impl.remove(2), is(equalTo(true)));
+	}
+	@When("deleting it, we should get replacement3.")
+	public void verify13(){
+		assertThat(impl.remove(1), is(equalTo(true)));
+	}
+	@Given("a root node with no left child,")
+	public void delete112231(){
+		impl.addNode(2, "root");
+		impl.addNode(10, "right");
+		impl.addNode(6, "root");
+	}
+	@When("deleting it, we should get replacement4.")
+	public void verify14(){
+		assertThat(impl.remove(2), is(equalTo(true)));
+	}
+	@Given("a left node with no left child,")
+	public void delete11212231(){
+		impl.addNode(2, "root");
+		impl.addNode(1, "left");
+		impl.addNode(10, "right");
+		impl.addNode(0, "root");
+		impl.addNode(6, "root");
+	}
+	@When("deleting it, we should get replacement5.")
+	public void verify15(){
+		assertThat(impl.remove(0), is(equalTo(true)));
+		assertThat(impl.remove(1), is(equalTo(true)));
+		assertThat(impl.remove(2), is(equalTo(true)));
 	}
 }
--- BinaryTreeJBehave/stories/BinaryTreeStory.story
+++ BinaryTreeJBehave/stories/BinaryTreeStory.story
@@ -1,22 +1,57 @@
 Scenario: BinaryTree AddNode
 Given a first node to tree,
 When added to tree it becomes root.
-
+Scenario: BinaryTree AddNode2
 Given a third node to tree,
 When added to tree it becomes right child.
-
+Scenario: BinaryTree AddNode3
 Given a second node to tree,
 When added to tree it becomes left child.
-
+Scenario: BinaryTree Find1
 Given a node to find,
 When finding root should become focusNode.
-
+Scenario: BinaryTree Find2
 Given a node different than root to find,
 When finding it, it should have different key than root.
-
-
+Scenario: BinaryTree Find3
 Given a node to find which doesnt exsist in tree,
 When finding it, it shhould return null.
-
+Scenario: BinaryTree Find4
 Given a node to find with lower key than focusNode,
 When fining it, it should start finding in leftChild.
+Scenario: BinaryTree Delete1
+Given a node to delete from tree, which doesnt exsist in,
+When deleting it, remove method should return false.
+Scenario: BinaryTree Delete2
+Given a node to delete from tree,
+When deleting it, remove method should return true.
+Scenario: BinaryTree Delete3
+Given a node to delete from tree,
+When deleting it, we shouldnt find it in tree with findNode method.
+Scenario: BinaryTree Delete4
+Given a root to delete from tree,
+When deleting it, we should find null tree.
+Scenario: BinaryTree Delete5
+Given a left node with no right child,
+When deleting it should be deleted.
+Scenario: BinaryTree Delete6
+Given a node with children to delete,
+When deleting it should make replecament.
+Scenario: BinaryTree Delete7
+Given a left node with no child to delete,
+When deleting it, we should get true from method.
+Scenario: BinaryTree Delete8
+Given a right node with no child to delete,
+When deleting it, we should get replacement.
+Scenario: BinaryTree Delete9
+Given a right node with no child to delete,
+When deleting it, we should get replacement2.
+Scenario: BinaryTree Delete10
+Given a right node with no child to delete,
+When deleting it, we should get replacement3.
+Scenario: BinaryTree Delete11
+Given a root node with no left child,
+When deleting it, we should get replacement4.
+Scenario: BinaryTree Delete11
+Given a left node with no left child,
+When deleting it, we should get replacement5.
