d81b96008da4f4370e22b5f032505ad3c95e986d
Patryk Sadowski <patryk.sadowski113@gmail.com>
2016-04-12 19:42:13 +0200

    chyba wersja finale

--- BinaryTreeJBehave/src/main/java/BinaryTree/BinaryTreeJBehave/BinaryTree.java
+++ BinaryTreeJBehave/src/main/java/BinaryTree/BinaryTreeJBehave/BinaryTree.java
@@ -2,6 +2,7 @@ package BinaryTree.BinaryTreeJBehave;
 
 public interface BinaryTree {
 	public void addNode(int key, String name);
-
+	public boolean remove(int key);
 	public Node findNode(int key);
+	public Node getReplacementNode(Node replacedNode);
 }
--- BinaryTreeJBehave/src/main/java/BinaryTree/BinaryTreeJBehave/BinaryTreeImpl.java
+++ BinaryTreeJBehave/src/main/java/BinaryTree/BinaryTreeJBehave/BinaryTreeImpl.java
@@ -45,10 +45,76 @@ public class BinaryTreeImpl implements BinaryTree {
 		}
 		return focusNode;
 	}
+
+	public boolean remove(int key) {
+		Node focusNode = root;
+		Node parent = root;
+		boolean isItALeftChild = true;
+		while (focusNode.key != key) {
+			parent = focusNode;
+			if (key < focusNode.key) {
+				isItALeftChild = true;
+				focusNode = focusNode.leftChild;
+			} else {
+				isItALeftChild = false;
+				focusNode = focusNode.rightChild;
+			}
+			if (focusNode == null)
+				return false;
+
+		}
+		if (focusNode.leftChild == null && focusNode.rightChild == null) {
+			if (focusNode == root)
+				root = null;
+			else if (isItALeftChild)
+				parent.leftChild = null;
+			else
+				parent.rightChild = null;
+		} else if (focusNode.rightChild == null) {
+			if (focusNode == root)
+				root = focusNode.leftChild;
+			else if (isItALeftChild)
+				parent.leftChild = focusNode.leftChild;
+			else
+				parent.rightChild = focusNode.leftChild;
+		} else if (focusNode.leftChild == null) {
+			if (focusNode == root)
+				root = focusNode.rightChild;
+			else if (isItALeftChild)
+				parent.leftChild = focusNode.rightChild;
+			else
+				parent.rightChild = focusNode.rightChild;
+		} else {
+			Node replacement = getReplacementNode(focusNode);
+			if (focusNode == root)
+				root = replacement;
+			else if (isItALeftChild)
+				parent.leftChild = replacement;
+			else
+				parent.rightChild = replacement;
+			replacement.leftChild = focusNode.leftChild;
+		}
+		return true;
+	}
+
+	public Node getReplacementNode(Node replacedNode) {
+		Node replacementParent = replacedNode;
+		Node replacement = replacedNode;
+		Node focusNode = replacedNode.rightChild;
+		while (focusNode != null) {
+			replacementParent = replacement;
+			replacement = focusNode;
+			focusNode = focusNode.leftChild;
+		}
+		if (replacement != replacedNode.rightChild) {
+			replacementParent.leftChild = replacement.rightChild;
+			replacement.rightChild = replacedNode.rightChild;
+		}
+		return replacement;
+	}
 }
 
 class Node {
-
 	int key;
 	String name;
 	Node leftChild;
